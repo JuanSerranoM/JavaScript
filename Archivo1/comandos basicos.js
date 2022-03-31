ea4c1b2a8365a974f87cf5cb63521e6993742875
JuanSerranoM <juancruzserrano.m@gmail.com>
2022-03-31 01:36:36 -0300

    Creacion de una hoja css, dos botones con js

--- Archivo1/comandos basicos.js
+++ Archivo1/comandos basicos.js
@@ -1,3 +1,19 @@
-function RespuestaAutomatica(){
+function respuestaAutomatica(){
     document.write("<h1>Prueba del primer boton con JavaScript en VisualStudio</h1>");
+    document.write('<input type="button" value="volver atras" onclick="location.reload()">');
+}
+
+function agregarTexto(){
+    let div = document.getElementById("campoVacio");
+    let nombre = prompt("Ingrese su nombre: ","Desconocido");
+    const elementoP = document.createElement("p");
+
+    if(nombre == "Desconocido"){
+        elementoP.textContent = "No llenaste el espacio con tu nombre entonces te llamaremos " + nombre;
+    }else{
+        elementoP.textContent = "Hola " + nombre + ", gracias por decirnos tu nombre!"
+    }
+
+    div.appendChild(elementoP);
+
 }
