9ff4177c6d36a381fdd17aa3c1a0241ce0819b85
JuanSerranoM <juancruzserrano.m@gmail.com>
2022-04-06 01:57:58 -0300

    Se agregó una tabla para jugar al tutti frutti

--- Archivo1/comandos basicos.js
+++ Archivo1/comandos basicos.js
@@ -15,5 +15,27 @@ function agregarTexto(){
     }
 
     div.appendChild(elementoP);
-
 }
+
+function comenzarJuego(){
+    let tabla = document.getElementById("tabla");
+    let fila = document.createElement("tr");
+    fila.className = "fila";
+    for(let i = 0; i < 6; i++){
+        let celda = document.createElement("th");
+        let campo = document.createElement("input");
+        campo.type = "text";
+        campo.className = "campo";
+        celda.appendChild(campo);
+        fila.appendChild(celda);
+    } 
+    tabla.appendChild(fila);
+}
+
+function resetJuego(){
+    let tabla = document.getElementById("tabla");
+    while (tabla.lastChild && tabla.lastChild.className == "fila") {
+        tabla.removeChild(tabla.lastChild);
+    }
+}
+
